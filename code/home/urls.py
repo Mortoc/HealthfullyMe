d6c328500ec1e974412bd0d30fa41a29ab6d04fa
Mortoc <Mortoc@gmail.com>
2013-02-06 18:16:20 -0500

    Did the layout for the malanding page

--- code/home/urls.py
+++ code/home/urls.py
@@ -5,4 +5,5 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'home.views.index', name='index'),
+    url(r'^/submit_email_request/', 'home.views.submit_email_request', name='submit_email_request')
 )
--- code/home/views.py
+++ code/home/views.py
@@ -1,8 +1,20 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.template import RequestContext, loader
+from home.models import EmailRequest
 
 
 def index(request):
-    template = loader.get_template("index.html")
-    context = RequestContext(request, {})
-    return HttpResponse( template.render(context) )
+
+    if request.method == 'POST': # If the form has been submitted...
+        form = EmailRequest(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = EmailRequest() # An unbound form
+
+    return render(request, "index.html", {
+       'form' : form                                   
+    });
