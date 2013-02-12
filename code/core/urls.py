9a894df06dc2f70b32d481f890d2d01f884c373a
Mortoc <Mortoc@gmail.com>
2013-02-12 18:53:47 -0500

    Last iteration of mobile dedesign for now

--- code/core/urls.py
+++ code/core/urls.py
@@ -4,12 +4,13 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', '/static/favicon.ico'),
+    
     url(r'^$', 'home.views.index', name='index'),
     url(r'^submit-email', 'home.views.submit_email_request', name='submit-request'),
     url(r'^email-success', 'home.views.email_submission_success', name='submission-success'),
     url(r'^email-failure', 'home.views.email_submission_failure', name='submission-failure'),
 
-    url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/images/favicon.ico'}),
     
     url(r'^admin/', include(admin.site.urls)),
 )
