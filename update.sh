7f567790f8192a95f4f4f60c54a9fa59687c26a1
Mortoc <Mortoc@gmail.com>
2013-02-12 13:25:53 -0500

    Added an update script

---
+++ update.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+ROOT_UID="0"
+
+# Check if run as root
+if [ "$UID" -ne "$ROOT_UID" ] ; then
+	echo "update reqires root access"
+	exit 1
+fi
+
+# Update from git
+git stash
+git pull
+
+cd code
+./manage.py collectstatic
+./manage.py syncdb
