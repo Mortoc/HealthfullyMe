6881fcd82ee277f80122a6acaa92c5d0a6330cfa
Mortoc <Mortoc@gmail.com>
2013-03-05 23:41:47 -0500

    added a primaryid to core.models.Address

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Address(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=256, null=True)
     line1 = models.CharField(max_length=256)
     line2 = models.CharField(max_length=256, null=True)
