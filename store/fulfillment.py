2c37c283a7b95c295e575beee753b4f21cb34a6d
Mortoc <Mortoc@gmail.com>
2013-04-15 14:53:03 -0400

    fixed order id in inventory empty email

--- store/fulfillment.py
+++ store/fulfillment.py
@@ -56,6 +56,7 @@ def fulfill_egiftcard(transaction, send_failure_email=True):
                         "user" : transaction.user,
                         "transaction" : transaction,
                         "hostname" : settings.HOSTNAME
+                        "offer" : transaction.offer
                     }
                 )
                 user_email = message_from_template(
